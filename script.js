57a31777e91d81c3a830878f9337105589bb574b
Jiroma <37752352+Jiroma@users.noreply.github.com>
2023-11-25 19:32:51 -0300

    Add export to excel (#27)
    * Fix second discount
    
    * Fix dropdown Minecraft versions
    
    * Add export excel and comment discount1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const clp = {
     'bd': 200,
     'backup': 1000,
     'puertos': 500,
-    'ram': 1500,
+    'ram': 1500
 }
 //Valores de los componentes del servidor en dolares
 const usd = {
@@ -12,7 +12,7 @@ const usd = {
     'bd': 0.22,
     'backup': 1.11,
     'puertos': 0.56,
-    'ram': 1.67,
+    'ram': 1.67
 }
 //Cantidad base de cada uno de los componentes del servidor
 const base = {
@@ -20,7 +20,7 @@ const base = {
     'bd': 0,
     'backup': 0,
     'puertos': 1,
-    'ram': 1,//dudoso
+    'ram': 1
 }
 //Valores especiales de la ram
 const ram = {
@@ -29,7 +29,13 @@ const ram = {
     '3':1500,
     '4':2.22,//usd
     '5':0.055,
-    '6':1.67,
+    '6':1.67
+}
+
+const discounts = {
+    '0':1,
+    '4.5':3,
+    '10':12
 }
 
 let currency = "clp"
@@ -87,30 +93,20 @@ function actualizarEtiqueta2(input, label) {
 function calcularTotal(){
     const subtotales = document.querySelectorAll(".actualizar");
     let total = 0;
-    var e = document.getElementById("primerdescuento");
-    var discount1 = e.value;
 
-    var e = document.getElementById("primerdescuento");
-    var discount1 = e.value;
+    /* let e = document.getElementById("primerdescuento");
+    let discount1 = e.value; */
 
     e = document.getElementById("segundodescuento");
-    var discount2 = e.value;
+    let discount2 = e.value;
 
     subtotales.forEach(function(label){
         aux = label.textContent;
         aux = aux.slice(1);
         total += parseFloat(aux);
     });
-
-    var months=0;
-    if (discount2==20)
-        months=1;
-    else if (discount2==50)
-        months=3;
-    else
-        months=12;
-
-    total=total*(1-(discount1/100));
+    let months = discounts[discount2];
+    //total=total*(1-(discount1/100));
     total=total*(1-(discount2/100));
 
     total=total.toFixed(6)
@@ -121,7 +117,7 @@ function calcularTotal(){
 
     total=total*months;
     total=parseFloat(total).toFixed(2);
-    total="$"+total+". Cada mes se pagaría: $"+valueMonth+". Primer Descuento: "+discount1+"%. Segundo Descuento: "+discount2+"%.";
+    total="$"+total+". Cada mes se pagaría: $"+valueMonth+". " + /* "Primer Descuento: " + discount1 +"%." +   */"Segundo Descuento: "+discount2+"%.";
     
     document.getElementById("totalLabel").textContent="Total para "+months+" mes(es): "+total; 
 }
@@ -150,37 +146,41 @@ function crearComponente(cerrar = true) {
     componente.innerHTML = `
         <h2>Calculadora</h2>
         <div class="etiquetas">
-            <label class="texto">Servidor Minecraft :</label>
-            <select>
-                <option>Bedrock</option>
-                <option>gomint</option>
-                <option>LiteLoaderBDS</option>
-                <option>Nukkit</option>
-                <option>PocketMine MP</option>
-                <option>Cuberite</option>
-                <option>CurseForge</option>
-                <option>Fabric</option>
-                <option>Feather</option>
-                <option>Forge</option>
-                <option>Feed The Beast</option>
-                <option>Glowstone</option>
-                <option>Limbo</option>
-                <option>Krypton</option>
-                <option>Magma</option>
-                <option>Modrinth</option>
-                <option>Mohist</option>
-                <option>NanoLimbo</option>
-                <option>Paper</option>
-                <option>Folia</option>
-                <option>Purpur</option>
-                <option>Quilt</option>
-                <option>Spigot</option>
-                <option>SpongeForge</option>
-                <option>SpongeVanilla</option>
-                <option>Technic</option>
-                <option>VanillaCord</option>
-                <option>Bungeecord</option>
-                <option selected>Vanilla Minecraft</option>
+            <label class="textoS">Servidor Minecraft :</label>
+            <select class="versions">
+                <optgroup label="Bedrock">
+                    <option value="Bedrock-Bedrock">Bedrock</option>
+                    <option value="Bedrock-gomint">gomint</option>
+                    <option value="Bedrock-LiteLoaderBDS">LiteLoaderBDS</option>
+                    <option value="Bedrock-Nukkit">Nukkit</option>
+                    <option value="Bedrock-PocketMine">PocketMine MP</option>
+                </optgroup>
+                <optgroup label="Java">
+                    <option value="Java-Bungeecord">Bungeecord</option>
+                    <option value="Java-Cuberite">Cuberite</option>
+                    <option value="Java-CurseForge">CurseForge</option>
+                    <option value="Java-Fabric">Fabric</option>
+                    <option value="Java-Feather">Feather</option>
+                    <option value="Java-Forge">Forge</option>
+                    <option value="Java-Feed The Beast">Feed The Beast</option>
+                    <option value="Java-Glowstone">Glowstone</option>
+                    <option value="Java-Limbo">Limbo</option>
+                    <option value="Java-Krypton">Krypton</option>
+                    <option value="Java-Magma">Magma</option>
+                    <option value="Java-Modrinth">Modrinth</option>
+                    <option value="Java-Mohist">Mohist</option>
+                    <option value="Java-NanoLimbo">NanoLimbo</option>
+                    <option value="Java-Paper">Paper</option>
+                    <option value="Java-Folia">Folia</option>
+                    <option value="Java-Purpur">Purpur</option>
+                    <option value="Java-Quilt">Quilt</option>
+                    <option value="Java-Spigot">Spigot</option>
+                    <option value="Java-SpongeForge">SpongeForge</option>
+                    <option value="Java-SpongeVanilla">SpongeVanilla</option>
+                    <option value="Java-Technic">Technic</option>
+                    <option value="Java-VanillaCord">VanillaCord</option>                
+                    <option value="Java-Vanilla Minecraft" selected>Vanilla Minecraft</option>
+                </optgroup>
             </select>
         </div>
         <div class="etiquetas">
@@ -247,6 +247,65 @@ function setCurrency(){
     currency="usd";
 }
 
+function downloadExcel(){  
+    const opt = document.querySelectorAll("select.versions")
+    comps = opt.length;
+
+    let final = [];
+
+    entradas = document.querySelectorAll("input");
+    actualizaciones = document.querySelectorAll(".actualizar");
+    etiquetas = document.querySelectorAll(".texto");
+    let totaltotal = 0;    
+
+    for (let index = 0; index < comps; index++) {
+        final.push([opt[index].value, entradas[((5-1)+(index*5))].value + "GB RAM"]);
+        final.push(["Atributo","Cantidad","Precio"]);
+        let subt = 0
+        for(let abc  = 0+(5*index); abc < 5+(5*index); abc++){
+            let a = etiquetas[abc].textContent;
+            let b = Number(entradas[abc].value);
+            let c = actualizaciones[abc].textContent;
+            final.push([a,b,c]);
+            c = c.slice(1, -3);
+            subt = subt + Number(c);
+        }
+        final.push([,"Sub-Total Server", "$"+subt, "Por mes"]);
+        final.push([]);
+        totaltotal = totaltotal + subt;
+    }
+
+    final.push([,"Sub-total Final", "$" + totaltotal, "Por mes"]);
+
+    let dis2 = document.getElementById("segundodescuento").value;
+    let aux2 = dis2;
+    if(dis2 == 0){
+        dis2 = "1 mes";
+    }
+    else if(dis2 == 4.5){
+        dis2 = "3 meses";
+        totaltotal = totaltotal * 3;
+    }
+    else{
+        dis2 = "1 año";
+        totaltotal = totaltotal * 12;
+    }
+    
+    final.push([,"Sub-total Final", "$" + totaltotal, "Por "+ dis2]);    
+    console.log(totaltotal);
+    let descuento = totaltotal - ((1 - ((Number(aux2))/100)) * totaltotal);
+    final.push([, "Descuento", aux2 + "%", "$-" + descuento,"Descuento por " + dis2]);
+
+    final.push([]);
+    let finalfinal = totaltotal - descuento;
+    final.push([, "Precio Final", "$" + finalfinal, "Por " + dis2]);
+
+    const ws = XLSX.utils.aoa_to_sheet(final);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Calculos")
+    XLSX.writeFile(wb, "calculo.xlsx")
+}
+
 //Pone en order las funciones llamadas al inicio de la página
 function main(){
     // Agregar un componente inicial
@@ -271,6 +330,8 @@ function main(){
     document.getElementById("currency").addEventListener("change", changeCurrency);
     // Escucha los evento de click en el boton de calcular total
     document.getElementById("calcularTotal").addEventListener("click",calcularTotal);
+
+    document.getElementById("excel").addEventListener("click", downloadExcel);
 }
 
 main();
